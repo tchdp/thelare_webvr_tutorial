3468f2b8328d2f5d4133790f1d5753348e7303ad
chuy <chuy@chuys-MacBook-Pro.local>
2017-07-22 03:10:07 -0400

    adding more views

--- public/webVR.js
+++ public/webVR.js
@@ -1,28 +1,30 @@
-function onVrViewLoad() {
-  let vrView = new VRView.Player('#vrview', {
+var onViewLoad = function() {
+  var vrViewOne = new VRView.Player('#vrview-one', {
     image: 'https://i.imgur.com/L9FeGZI.jpg',
   });
 
-  let vrViewTwo = new VRView.Player('#vrview-two', {
+  var vrViewTwo = new VRView.Player('#vrview-two', {
     image: 'http://raw.githubusercontent.com/jsala1990/thelare_webvr_tutorial/master/public/assets/room-2.jpg',
   });
 
-  let vrViewThree = new VRView.Player('#vrview-three', {
+  var vrViewThree = new VRView.Player('#vrview-three', {
     image: 'http://raw.githubusercontent.com/jsala1990/thelare_webvr_tutorial/master/public/assets/room-5.jpg',
   });
 
-  // vrView.addHotspot('dining-room', {
-  //   pitch: 30, // In degrees. Up is positive.
-  //   yaw: 20, // In degrees. To the right is positive.
-  //   radius: 0.05, // Radius of the circular target in meters.
-  //   distance: 2, // Distance of target from camera in meters.
-  // });
+  vrView.addHotspot('dining-room', {
+    pitch: 30, // In degrees. Up is positive.
+    yaw: 20, // In degrees. To the right is positive.
+    radius: 0.05, // Radius of the circular target in meters.
+    distance: 2, // Distance of target from camera in meters.
+  });
 
-  // vrView.on('click', function(event) {
-  //   if (event.id == 'dining-room') {
-  //     // Handle hotspot click.
-  //   }
-  // });
+  vrViewTwo.on('click', function(event) {
+    if (event.id == 'dining-room') {
+      // Handle hotspot click.
+    }
+  });
+
+  alert('hello!!');
 }
 
-window.addEventListener('load', onVrViewLoad);
+window.addEventListener('load', onViewLoad);
